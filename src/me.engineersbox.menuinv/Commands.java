32fa15e2239ece4832a380df522f6713e62e4b4b
Jack Kilrain <toastcraft.info@gmail.com>
2018-01-19 13:32:16 +0100

    Merged changes to match EventList class

--- src/me.engineersbox.menuinv/Commands.java
+++ src/me.engineersbox.menuinv/Commands.java
@@ -14,74 +14,74 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class Commands implements CommandExecutor {
-
+	
 	public final static Inventory main = Bukkit.createInventory(null, 9, ChatColor.DARK_PURPLE + "[BlockPalette]");
 	
-		static {
-			
-			ItemStack exit = new ItemStack(Material.BEDROCK);
-            ItemMeta im0 = exit.getItemMeta();
-            im0.setDisplayName(ChatColor.GRAY + "EXIT");
-            exit.setItemMeta(im0);
-			
-			ItemStack plains = new ItemStack(Material.GRASS);
-            ItemMeta im = plains.getItemMeta();
-            im.setDisplayName(ChatColor.DARK_GREEN + "PLAINS");
-            ArrayList<String> Lore = new ArrayList<String>();
-            Lore.add(ChatColor.YELLOW + "Biome Palette");
-            im.setLore(Lore);
-            plains.setItemMeta(im);
-            
-            ItemStack desert = new ItemStack(Material.SAND);
-            ItemMeta im1 = desert.getItemMeta();
-            im1.setDisplayName(ChatColor.GOLD + "DESERT");
-            ArrayList<String> Lore1 = new ArrayList<String>();
-            Lore1.add(ChatColor.YELLOW + "Biome Palette");
-            im1.setLore(Lore1);
-            desert.setItemMeta(im1);
-            
-            ItemStack forest = new ItemStack(Material.LEAVES);
-            ItemMeta im2 = forest.getItemMeta();
-            im2.setDisplayName(ChatColor.GREEN + "FOREST");
-            ArrayList<String> Lore2 = new ArrayList<String>();
-            Lore2.add(ChatColor.YELLOW + "Biome Palette");
-            im2.setLore(Lore2);
-            forest.setItemMeta(im2);
-            
-            ItemStack extreme = new ItemStack(Material.STONE);
-            ItemMeta im3 = extreme.getItemMeta();
-            im3.setDisplayName(ChatColor.DARK_GRAY + "EXTREME HILLS");
-            ArrayList<String> Lore3 = new ArrayList<String>();
-            Lore3.add(ChatColor.YELLOW + "Biome Palette");
-            im3.setLore(Lore3);
-            extreme.setItemMeta(im3);
-            
-            ItemStack savanna = new ItemStack(Material.LOG_2);
-            ItemMeta im4 = savanna.getItemMeta();
-            im4.setDisplayName(ChatColor.DARK_RED + "SAVANNA");
-            ArrayList<String> Lore4 = new ArrayList<String>();
-            Lore4.add(ChatColor.YELLOW + "Biome Palette");
-            im4.setLore(Lore4);
-            savanna.setItemMeta(im4);
-            
-            ItemStack taiga = new ItemStack(Material.SNOW_BLOCK);
-            ItemMeta im5 = taiga.getItemMeta();
-            im5.setDisplayName(ChatColor.WHITE + "TAIGA");
-            ArrayList<String> Lore5 = new ArrayList<String>();
-            Lore5.add(ChatColor.YELLOW + "Biome Palette");
-            im5.setLore(Lore5);
-            taiga.setItemMeta(im5);
-			
-            main.setItem(0, plains);
-            main.setItem(1, desert);
-            main.setItem(2, forest);
-            main.setItem(3, extreme);
-            main.setItem(4, savanna);
-            main.setItem(5, taiga);
-            
-            main.setItem(8, exit);
-            
-		}
+	static {
+		
+		ItemStack exit = new ItemStack(Material.BEDROCK);
+        ItemMeta im0 = exit.getItemMeta();
+        im0.setDisplayName(ChatColor.GRAY + "EXIT");
+        exit.setItemMeta(im0);
+		
+		ItemStack plains = new ItemStack(Material.GRASS);
+        ItemMeta im = plains.getItemMeta();
+        im.setDisplayName(ChatColor.DARK_GREEN + "PLAINS");
+        ArrayList<String> Lore = new ArrayList<String>();
+        Lore.add(ChatColor.YELLOW + "Biome Palette");
+        im.setLore(Lore);
+        plains.setItemMeta(im);
+        
+        ItemStack desert = new ItemStack(Material.SAND);
+        ItemMeta im1 = desert.getItemMeta();
+        im1.setDisplayName(ChatColor.GOLD + "DESERT");
+        ArrayList<String> Lore1 = new ArrayList<String>();
+        Lore1.add(ChatColor.YELLOW + "Biome Palette");
+        im1.setLore(Lore1);
+        desert.setItemMeta(im1);
+        
+        ItemStack forest = new ItemStack(Material.LEAVES);
+        ItemMeta im2 = forest.getItemMeta();
+        im2.setDisplayName(ChatColor.GREEN + "FOREST");
+        ArrayList<String> Lore2 = new ArrayList<String>();
+        Lore2.add(ChatColor.YELLOW + "Biome Palette");
+        im2.setLore(Lore2);
+        forest.setItemMeta(im2);
+        
+        ItemStack extreme = new ItemStack(Material.STONE);
+        ItemMeta im3 = extreme.getItemMeta();
+        im3.setDisplayName(ChatColor.DARK_GRAY + "EXTREME HILLS");
+        ArrayList<String> Lore3 = new ArrayList<String>();
+        Lore3.add(ChatColor.YELLOW + "Biome Palette");
+        im3.setLore(Lore3);
+        extreme.setItemMeta(im3);
+        
+        ItemStack savanna = new ItemStack(Material.LOG_2);
+        ItemMeta im4 = savanna.getItemMeta();
+        im4.setDisplayName(ChatColor.DARK_RED + "SAVANNA");
+        ArrayList<String> Lore4 = new ArrayList<String>();
+        Lore4.add(ChatColor.YELLOW + "Biome Palette");
+        im4.setLore(Lore4);
+        savanna.setItemMeta(im4);
+        
+        ItemStack taiga = new ItemStack(Material.SNOW_BLOCK);
+        ItemMeta im5 = taiga.getItemMeta();
+        im5.setDisplayName(ChatColor.WHITE + "TAIGA");
+        ArrayList<String> Lore5 = new ArrayList<String>();
+        Lore5.add(ChatColor.YELLOW + "Biome Palette");
+        im5.setLore(Lore5);
+        taiga.setItemMeta(im5);
+		
+        main.setItem(0, plains);
+        main.setItem(1, desert);
+        main.setItem(2, forest);
+        main.setItem(3, extreme);
+        main.setItem(4, savanna);
+        main.setItem(5, taiga);
+        
+        main.setItem(8, exit);
+        
+	}
 		
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
