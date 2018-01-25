d0814566857eb2cc36f5aab3dd390d09bf4a0e21
Jack Kilrain <toastcraft.info@gmail.com>
2018-01-25 02:42:09 +0100

    added command executor for bp tool

--- src/me.engineersbox.menuinv/Main.java
+++ src/me.engineersbox.menuinv/Main.java
@@ -4,11 +4,13 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-	 
+
     @Override
     public void onEnable() {
         Bukkit.getServer().getPluginManager().registerEvents(new EventList(), this);
         getCommand("bp").setExecutor(new Commands());
+        getCommand("bp help").setExecutor(new Commands());
+        getCommand("bp tool").setExecutor(new Commands());
     }
  
     @Override
