40afebbead4db7732f036fe63ea27daae0e307cc
Jack Kilrain <toastcraft.info@gmail.com>
2018-01-27 05:56:34 +0100

    added variable for biome + biome command

--- src/me.engineersbox.menuinv/Main.java
+++ src/me.engineersbox.menuinv/Main.java
@@ -4,18 +4,24 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-
+	
+	public static String biome = "";
+	
     @Override
     public void onEnable() {
+    	
         Bukkit.getServer().getPluginManager().registerEvents(new EventList(), this);
+        
         getCommand("bp").setExecutor(new Commands());
         getCommand("bp help").setExecutor(new Commands());
         getCommand("bp tool").setExecutor(new Commands());
+        getCommand("bp tool biome").setExecutor(new Commands());
+        getCommand("bp version").setExecutor(new Commands());
     }
  
     @Override
     public void onDisable() {
      
     }
- 
+    
 }
