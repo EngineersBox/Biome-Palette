c92fe2698aaaa13aa2eb72269d1cfb8078b740dd
Jack Kilrain <toastcraft.info@gmail.com>
2018-02-01 09:37:05 +0100

    Added /bp settings with conditionals for chunkinfo and getbiome

--- src/me.engineersbox.menuinv/Commands.java
+++ src/me.engineersbox.menuinv/Commands.java
@@ -35,7 +35,9 @@ public class Commands implements CommandExecutor {
 	                	p.sendMessage(ChatColor.BLACK + "> " + ChatColor.DARK_GREEN + "/bp " + ChatColor.WHITE + ":: " + ChatColor.DARK_RED + "Opens The Block Palette Interface");
 	                	p.sendMessage(ChatColor.BLACK + "> " + ChatColor.DARK_GREEN + "/bp tool " + ChatColor.WHITE + ":: " + ChatColor.DARK_RED + "Gives The Block Biome Altering Tool");
 	                	p.sendMessage(ChatColor.BLACK + "> " + ChatColor.DARK_GREEN + "/bp setbiome <biome>" + ChatColor.WHITE + ":: " + ChatColor.DARK_RED + "Sets The Biome To Be Used with /bp tool");
+	                	p.sendMessage(ChatColor.BLACK + "> " + ChatColor.DARK_GREEN + "/bp getbiome <enable/disable> " + ChatColor.WHITE + ":: " + ChatColor.DARK_RED + "Changes Tool To Identify The Biome Of A Selected Block");
 	                	p.sendMessage(ChatColor.BLACK + "> " + ChatColor.DARK_GREEN + "/bp biomelist " + ChatColor.WHITE + ":: " + ChatColor.DARK_RED + "Displays The Valid Biomes For /bp tool biome");
+	                	p.sendMessage(ChatColor.BLACK + "> " + ChatColor.DARK_GREEN + "/bp chunkinfo <enable/disable> " + ChatColor.WHITE + ":: " + ChatColor.DARK_RED + "Displays Chunk Data When Using /bp tool");
 	                	p.sendMessage(ChatColor.BLACK + "> " + ChatColor.DARK_GREEN + "/bp version " + ChatColor.WHITE + ":: " + ChatColor.DARK_RED + "Displays The Plugin Version And Author");
 	                	p.sendMessage(ChatColor.BLACK + "> " + ChatColor.DARK_GREEN + "/bp reload " + ChatColor.WHITE + ":: " + ChatColor.DARK_RED + "Reloads The BlockPalette Plugin");
 	                	p.sendMessage(ChatColor.BLACK + "> " + ChatColor.DARK_GREEN + "/bp help " + ChatColor.WHITE + ":: " + ChatColor.DARK_RED + "Opens This Menu");
@@ -49,6 +51,7 @@ public class Commands implements CommandExecutor {
             			p.getInventory().addItem(Item.tool);
             			
             		} else if (args[0].equalsIgnoreCase("setbiome")) {
+            			
             			if (args.length == 2) {
             				
             				if ((args[1].equalsIgnoreCase("desert")) || (args[1].equalsIgnoreCase("forest")) || (args[1].equalsIgnoreCase("ocean")) || (args[1].equalsIgnoreCase("extremehills")) || (args[1].equalsIgnoreCase("taiga")) || (args[1].equalsIgnoreCase("plains"))) {
@@ -69,9 +72,123 @@ public class Commands implements CommandExecutor {
             				Main.biome = "";
             				p.sendMessage(Main.prefix + ChatColor.DARK_PURPLE + "Invalid Syntax!");
             				p.sendMessage(Main.prefix + ChatColor.DARK_PURPLE + "Usage: " + ChatColor.ITALIC + "/bp setbiome <biome>");
+            	
+            			}
+            			
+            		
+            		} else if (args[0].equalsIgnoreCase("getbiome")) {
+            			
+            			if (args.length == 2) {
+            				
+            				if (args[1].equalsIgnoreCase("enable")) {
+            					
+            					if (Main.getB == 0) {
+            						
+            						Main.getB = 1;
+            						p.sendMessage(Main.prefix + ChatColor.DARK_AQUA + "Get Biome Enabled!");
+            						
+            					} else if (Main.getB == 1) {
+            						
+            						p.sendMessage(Main.prefix + ChatColor.DARK_PURPLE + "Error: Get Biome Already Enabled!");
+            						
+            					}
+            					
+            					
+            				} else if (args[1].equalsIgnoreCase("disable")) {
+            					
+            					if (Main.getB == 1) {
+            						
+            						Main.getB = 0;
+            						p.sendMessage(Main.prefix + ChatColor.DARK_AQUA + "Get Biome Disabled!");
+            						
+            					} else if (Main.getB == 0) {
+            						
+            						p.sendMessage(Main.prefix + ChatColor.DARK_PURPLE + "Error: Get Biome Already Disabled!");
+            						
+            					}
+            					
+            				} else {
+            					
+            					p.sendMessage(Main.prefix + ChatColor.DARK_PURPLE + "Invalid Syntax!");
+                				p.sendMessage(Main.prefix + ChatColor.DARK_PURPLE + "Usage: " + ChatColor.ITALIC + "/bp getbiome <enable/disable>");
+            					
+            				}
             				
             			}
             			
+            		} else if (args[0].equalsIgnoreCase("chunkinfo")) {
+            			
+            			if (args.length == 2) {
+            				
+            				if (args[1].equalsIgnoreCase("enable")) {
+            					
+            					if (Main.chunkinfo == 0) {
+            						
+            						Main.chunkinfo = 1;
+            						p.sendMessage(Main.prefix + ChatColor.DARK_AQUA + "Chunk Info Enabled!");
+            						
+            					} else if (Main.chunkinfo == 1) {
+            						
+            						p.sendMessage(Main.prefix + ChatColor.DARK_PURPLE + "Error: Chunk Info Already Enabled!");
+            						
+            					}
+            					
+            					
+            				} else if (args[1].equalsIgnoreCase("disable")) {
+            					
+            					if (Main.chunkinfo == 1) {
+            						
+            						Main.chunkinfo = 0;
+            						p.sendMessage(Main.prefix + ChatColor.DARK_AQUA + "Chunk Info Disabled!");
+            						
+            					} else if (Main.chunkinfo == 0) {
+            						
+            						p.sendMessage(Main.prefix + ChatColor.DARK_PURPLE + "Error: Chunk Info Already Disabled!");
+            						
+            					}
+            					
+            				} else {
+            					
+            					p.sendMessage(Main.prefix + ChatColor.DARK_PURPLE + "Invalid Syntax!");
+                				p.sendMessage(Main.prefix + ChatColor.DARK_PURPLE + "Usage: " + ChatColor.ITALIC + "/bp chunkinfo <enable/disable>");
+            					
+            				}
+            				
+            			}
+            		
+            		} else if (args[0].equalsIgnoreCase("settings")) {
+            			
+            			String getbiome = "";
+            			String chunkinfo = "";
+            			
+            			if (Main.getB == 1) {
+            				
+            				getbiome = ChatColor.GREEN + "Enabled";
+            				
+            			} else {
+            				
+            				getbiome = ChatColor.DARK_RED + "Disabled";
+            				
+            			}
+            			
+            			if (Main.chunkinfo == 1) {
+            				
+            				chunkinfo = ChatColor.GREEN + "Enabled";
+            				
+            			} else {
+            				
+            				chunkinfo = ChatColor.DARK_RED + "Disabled";
+            				
+            			}
+            			
+            			p.sendMessage("");
+        		    	p.sendMessage(ChatColor.DARK_GRAY + "----={<" + ChatColor.DARK_RED + "  [" + ChatColor.GOLD + "BlockPalette Settings" + ChatColor.DARK_RED + "]  " + ChatColor.DARK_GRAY + "}>=----");
+        		    	p.sendMessage("");
+        		    	p.sendMessage(ChatColor.BLACK + "> " + ChatColor.DARK_GREEN + "Get Biome " + ChatColor.WHITE + ":: " + getbiome);
+        		    	p.sendMessage(ChatColor.BLACK + "> " + ChatColor.DARK_GREEN + "Chunk Info " + ChatColor.WHITE + ":: " + chunkinfo);
+        		    	p.sendMessage("");
+        		    	p.sendMessage(ChatColor.DARK_GRAY + "----=<{" + ChatColor.DARK_RED + "  [" + ChatColor.GOLD + "BlockPalette Settings" + ChatColor.DARK_RED + "]  " + ChatColor.DARK_GRAY + "}>=----");
+        		    	p.sendMessage("");
             			
             		} else if (args[0].equalsIgnoreCase("version")) {
         				
