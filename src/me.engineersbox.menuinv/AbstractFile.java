f457afd9d8b029450278043b4b23c9da084dc9e5
Jack Kilrain <toastcraft.info@gmail.com>
2018-03-17 02:16:52 +0100

    Resolved static references to allow class referencing

--- src/me.engineersbox.menuinv/AbstractFile.java
+++ src/me.engineersbox.menuinv/AbstractFile.java
@@ -8,30 +8,31 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import me.engineersbox.menuinv.Main;
 
 public class AbstractFile {
-	
-	protected Main main;
-	private static File file;
-	protected static FileConfiguration config;
-	
-	public AbstractFile(Main main, String filename) {
-		this.main = main;
-		AbstractFile.file = new File(main.getDataFolder(), filename);
-		if (!file.exists()) {
-			try {
-				file.createNewFile();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		AbstractFile.config = YamlConfiguration.loadConfiguration(file);
-	}
-	
-	public static void save() {
-		try {
-			config.save(file);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
+   
+    protected static Main main;
+    static File file;
+    protected static FileConfiguration config;
+   
+    public AbstractFile(Main main, String filename) {
+        AbstractFile.main = main;
+        AbstractFile.file = new File(main.getDataFolder(), filename);
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        AbstractFile.config = YamlConfiguration.loadConfiguration(file);
+        config.options().copyDefaults(true);
+    }
+   
+    public static void saveConfig() {
+        try {
+            config.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+   
 }
