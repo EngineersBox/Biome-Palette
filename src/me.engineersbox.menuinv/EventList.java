f2eef5c5adcec7053874eae7190473e8e2d8d6a0
Jack Kilrain <toastcraft.info@gmail.com>
2018-05-22 11:38:46 +0200

    Fixed a NullPointerException with getting an obsidian item in an inventory

--- src/me.engineersbox.menuinv/EventList.java
+++ src/me.engineersbox.menuinv/EventList.java
@@ -110,194 +110,196 @@ public class EventList implements Listener {
             //p.sendMessage("Event Cancelled!"); //debug
         }
         
-        if (e.getCurrentItem() == null) {
-        	
-        	return;
-        	
-        } else if ((e.getCurrentItem().getType() == Material.OBSIDIAN) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.GRAY + "EXIT"))) {
-        	
-        	exitflag = true;
-        	Main.cinv = 0;
-        	Main.i = 0;
-        	p.closeInventory();
-        	
-        } else if ((e.getCurrentItem().getType() == Material.BOOK) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.DARK_RED + "BACK TO MAIN"))) {
-    		
-    		exitflag = true;
-        	Main.cinv = 0;
-        	Main.i = 0;
-        	p.openInventory(Inventories.main);
-        
+        if (e.getInventory().getTitle().contains(Main.prefix)) {
+	        if ((e.getCurrentItem() == null) || (e.getCurrentItem().getItemMeta() == null)) {
+	        	
+	        	return;
+	        	
+	        } else if ((e.getCurrentItem().getType() == Material.OBSIDIAN) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.GRAY + "EXIT"))) {
+	        	
+	        	exitflag = true;
+	        	Main.cinv = 0;
+	        	Main.i = 0;
+	        	p.closeInventory();
+	        	
+	        } else if ((e.getCurrentItem().getType() == Material.BOOK) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.DARK_RED + "BACK TO MAIN"))) {
+	    		
+	    		exitflag = true;
+	        	Main.cinv = 0;
+	        	Main.i = 0;
+	        	p.openInventory(Inventories.main);
+	        
+	        }
+	        
+	        if (e.getInventory().getTitle().equals(ChatColor.DARK_RED + "           " + Main.prefix)) { //main inventory
+	            
+	            if (e.getCurrentItem().getItemMeta() != null) {
+	            	
+	            	String currentItem = ChatColor.stripColor(e.getCurrentItem().getItemMeta().getDisplayName().toLowerCase());
+	            	
+	            	if (Main.BiomeList.contains(currentItem)) {
+	            		
+	            		Main.i = 0;
+	            		Inventories.openStoredInv(e, currentItem);
+	            		
+	            	} else if (exitflag == true) {
+	            		
+	            		exitflag = false;
+	            		return;
+	            		
+	            	} else {
+	            		
+	            		p.sendMessage(Main.prefix + ChatColor.DARK_PURPLE + "Invalid Biome!");
+	            		
+	            	}
+	            	
+	            } else {
+	            	
+	            	return;
+	            	
+	            }
+	            
+	            if (e.getCurrentItem() == null) {
+	            	
+	            	return;
+	            	
+	            }
+	        }
+	
+	        
+	        
+	        if (e.getInventory().getTitle().equals(Main.prefix + ChatColor.GOLD + "Desert")) { //desert inventory
+	            
+	            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.DARK_GREEN + "Next Page")) && (Main.oinv == true)) {
+	            	Main.cinv += 1;
+	            	LoadInv.setItemList("desert");
+	            	p.openInventory(Inventories.desert);
+	            	//p.sendMessage("Completed " + Main.oinv);
+	            }
+	            
+	            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.AQUA + "Previous Page")) && (Main.cinv > 0)) {
+	            	Main.cinv -= 1;
+	            	Main.i = 36 * Main.cinv;
+	            	LoadInv.setItemList("desert");
+	            	p.openInventory(Inventories.desert);
+	            }
+	        
+	        }
+	        
+	        
+	        if (e.getInventory().getTitle().equals(Main.prefix + ChatColor.DARK_GREEN + "Plains")) { //plains inventory
+	        	
+	            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.DARK_GREEN + "Next Page")) && (Main.oinv == true)) {
+	            	Main.cinv += 1;
+	            	LoadInv.setItemList("plains");
+	            	p.openInventory(Inventories.plains);
+	            	//p.sendMessage("Completed " + Main.oinv);
+	            }
+	            
+	            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.AQUA + "Previous Page")) && (Main.cinv > 0)) {
+	            	Main.cinv -= 1;
+	            	Main.i = 36 * Main.cinv;
+	            	LoadInv.setItemList("plains");
+	            	p.openInventory(Inventories.plains);
+	            }
+	        
+	        }
+	        
+	        if (e.getInventory().getTitle().equals(Main.prefix + ChatColor.GREEN + "Forest")) { //forest inventory
+	            
+	            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.DARK_GREEN + "Next Page")) && (Main.oinv == true)) {
+	            	Main.cinv += 1;
+	            	LoadInv.setItemList("forest");
+	            	p.openInventory(Inventories.forest);
+	            	//p.sendMessage("Completed " + Main.oinv);
+	            }
+	            
+	            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.AQUA + "Previous Page")) && (Main.cinv > 0)) {
+	            	Main.cinv -= 1;
+	            	Main.i = 36 * Main.cinv;
+	            	LoadInv.setItemList("forest");
+	            	p.openInventory(Inventories.forest);
+	            }
+	        
+	        }
+	        
+	        
+	        if (e.getInventory().getTitle().equals(Main.prefix + ChatColor.DARK_GRAY + "Extreme Hills")) { //extreme hills inventory
+	            
+	            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.DARK_GREEN + "Next Page")) && (Main.oinv == true)) {
+	            	Main.cinv += 1;
+	            	LoadInv.setItemList("extremehills");
+	            	p.openInventory(Inventories.extremehills);
+	            	//p.sendMessage("Completed " + Main.oinv);
+	            }
+	            
+	            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.AQUA + "Previous Page")) && (Main.cinv > 0)) {
+	            	Main.cinv -= 1;
+	            	Main.i = 36 * Main.cinv;
+	            	LoadInv.setItemList("extremehills");
+	            	p.openInventory(Inventories.extremehills);
+	            }
+	        
+	        }
+	        
+	        
+	        if (e.getInventory().getTitle().equals(Main.prefix + ChatColor.DARK_RED + "Savanna")) { //savanna inventory
+	            
+	            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.DARK_GREEN + "Next Page")) && (Main.oinv == true)) {
+	            	Main.cinv += 1;
+	            	LoadInv.setItemList("savanna");
+	            	p.openInventory(Inventories.savanna);
+	            	//p.sendMessage("Completed " + Main.oinv);
+	            }
+	            
+	            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.AQUA + "Previous Page")) && (Main.cinv > 0)) {
+	            	Main.cinv -= 1;
+	            	Main.i = 36 * Main.cinv;
+	            	LoadInv.setItemList("savanna");
+	            	p.openInventory(Inventories.savanna);
+	            }
+	        
+	        }
+	        
+	        
+	        if (e.getInventory().getTitle().equals(Main.prefix + ChatColor.WHITE + "Taiga")) { //taiga inventory
+	            
+	            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.DARK_GREEN + "Next Page")) && (Main.oinv == true)) {
+	            	Main.cinv += 1;
+	            	LoadInv.setItemList("taiga");
+	            	p.openInventory(Inventories.taiga);
+	            	//p.sendMessage("Completed " + Main.oinv);
+	            }
+	            
+	            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.AQUA + "Previous Page")) && (Main.cinv > 0)) {
+	            	Main.cinv -= 1;
+	            	Main.i = 36 * Main.cinv;
+	            	LoadInv.setItemList("taiga");
+	            	p.openInventory(Inventories.taiga);
+	            }
+	        
+	        }
+	        
+	        
+	        if (e.getInventory().getTitle().equals(Main.prefix + ChatColor.DARK_BLUE + "Ocean")) { //ocean inventory
+	            
+	            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.DARK_GREEN + "Next Page")) && (Main.oinv == true)) {
+	            	Main.cinv += 1;
+	            	LoadInv.setItemList("ocean");
+	            	p.openInventory(Inventories.ocean);
+	            	//p.sendMessage("Completed " + Main.oinv);
+	            }
+	            
+	            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.AQUA + "Previous Page")) && (Main.cinv > 0)) {
+	            	Main.cinv -= 1;
+	            	Main.i = 36 * Main.cinv;
+	            	LoadInv.setItemList("ocean");
+	            	p.openInventory(Inventories.ocean);
+	            }
+	        
+	        }
         }
-        
-        if (e.getInventory().getTitle().equals(ChatColor.DARK_RED + "           " + Main.prefix)) { //main inventory
-            
-            if (e.getCurrentItem().getItemMeta() != null) {
-            	
-            	String currentItem = ChatColor.stripColor(e.getCurrentItem().getItemMeta().getDisplayName().toLowerCase());
-            	
-            	if (Main.BiomeList.contains(currentItem)) {
-            		
-            		Main.i = 0;
-            		Inventories.openStoredInv(e, currentItem);
-            		
-            	} else if (exitflag == true) {
-            		
-            		exitflag = false;
-            		return;
-            		
-            	} else {
-            		
-            		p.sendMessage(Main.prefix + ChatColor.DARK_PURPLE + "Invalid Biome!");
-            		
-            	}
-            	
-            } else {
-            	
-            	return;
-            	
-            }
-            
-            if (e.getCurrentItem() == null) {
-            	
-            	return;
-            	
-            }
-        }
-
-        
-        
-        if (e.getInventory().getTitle().equals(Main.prefix + ChatColor.GOLD + "Desert")) { //desert inventory
-            
-            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.DARK_GREEN + "Next Page")) && (Main.oinv == true)) {
-            	Main.cinv += 1;
-            	LoadInv.setItemList("desert");
-            	p.openInventory(Inventories.desert);
-            	//p.sendMessage("Completed " + Main.oinv);
-            }
-            
-            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.AQUA + "Previous Page")) && (Main.cinv > 0)) {
-            	Main.cinv -= 1;
-            	Main.i = 36 * Main.cinv;
-            	LoadInv.setItemList("desert");
-            	p.openInventory(Inventories.desert);
-            }
-        
-        }
-        
-        
-        if (e.getInventory().getTitle().equals(Main.prefix + ChatColor.DARK_GREEN + "Plains")) { //plains inventory
-        	
-            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.DARK_GREEN + "Next Page")) && (Main.oinv == true)) {
-            	Main.cinv += 1;
-            	LoadInv.setItemList("plains");
-            	p.openInventory(Inventories.plains);
-            	//p.sendMessage("Completed " + Main.oinv);
-            }
-            
-            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.AQUA + "Previous Page")) && (Main.cinv > 0)) {
-            	Main.cinv -= 1;
-            	Main.i = 36 * Main.cinv;
-            	LoadInv.setItemList("plains");
-            	p.openInventory(Inventories.plains);
-            }
-        
-        }
-        
-        if (e.getInventory().getTitle().equals(Main.prefix + ChatColor.GREEN + "Forest")) { //forest inventory
-            
-            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.DARK_GREEN + "Next Page")) && (Main.oinv == true)) {
-            	Main.cinv += 1;
-            	LoadInv.setItemList("forest");
-            	p.openInventory(Inventories.forest);
-            	//p.sendMessage("Completed " + Main.oinv);
-            }
-            
-            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.AQUA + "Previous Page")) && (Main.cinv > 0)) {
-            	Main.cinv -= 1;
-            	Main.i = 36 * Main.cinv;
-            	LoadInv.setItemList("forest");
-            	p.openInventory(Inventories.forest);
-            }
-        
-        }
-        
-        
-        if (e.getInventory().getTitle().equals(Main.prefix + ChatColor.DARK_GRAY + "Extreme Hills")) { //extreme hills inventory
-            
-            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.DARK_GREEN + "Next Page")) && (Main.oinv == true)) {
-            	Main.cinv += 1;
-            	LoadInv.setItemList("extremehills");
-            	p.openInventory(Inventories.extremehills);
-            	//p.sendMessage("Completed " + Main.oinv);
-            }
-            
-            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.AQUA + "Previous Page")) && (Main.cinv > 0)) {
-            	Main.cinv -= 1;
-            	Main.i = 36 * Main.cinv;
-            	LoadInv.setItemList("extremehills");
-            	p.openInventory(Inventories.extremehills);
-            }
-        
-        }
-        
-        
-        if (e.getInventory().getTitle().equals(Main.prefix + ChatColor.DARK_RED + "Savanna")) { //savanna inventory
-            
-            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.DARK_GREEN + "Next Page")) && (Main.oinv == true)) {
-            	Main.cinv += 1;
-            	LoadInv.setItemList("savanna");
-            	p.openInventory(Inventories.savanna);
-            	//p.sendMessage("Completed " + Main.oinv);
-            }
-            
-            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.AQUA + "Previous Page")) && (Main.cinv > 0)) {
-            	Main.cinv -= 1;
-            	Main.i = 36 * Main.cinv;
-            	LoadInv.setItemList("savanna");
-            	p.openInventory(Inventories.savanna);
-            }
-        
-        }
-        
-        
-        if (e.getInventory().getTitle().equals(Main.prefix + ChatColor.WHITE + "Taiga")) { //taiga inventory
-            
-            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.DARK_GREEN + "Next Page")) && (Main.oinv == true)) {
-            	Main.cinv += 1;
-            	LoadInv.setItemList("taiga");
-            	p.openInventory(Inventories.taiga);
-            	//p.sendMessage("Completed " + Main.oinv);
-            }
-            
-            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.AQUA + "Previous Page")) && (Main.cinv > 0)) {
-            	Main.cinv -= 1;
-            	Main.i = 36 * Main.cinv;
-            	LoadInv.setItemList("taiga");
-            	p.openInventory(Inventories.taiga);
-            }
-        
-        }
-        
-        
-        if (e.getInventory().getTitle().equals(Main.prefix + ChatColor.DARK_BLUE + "Ocean")) { //ocean inventory
-            
-            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.DARK_GREEN + "Next Page")) && (Main.oinv == true)) {
-            	Main.cinv += 1;
-            	LoadInv.setItemList("ocean");
-            	p.openInventory(Inventories.ocean);
-            	//p.sendMessage("Completed " + Main.oinv);
-            }
-            
-            if ((e.getCurrentItem().getType() == Material.PAPER) && (e.getCurrentItem().getItemMeta().getDisplayName().equals(ChatColor.AQUA + "Previous Page")) && (Main.cinv > 0)) {
-            	Main.cinv -= 1;
-            	Main.i = 36 * Main.cinv;
-            	LoadInv.setItemList("ocean");
-            	p.openInventory(Inventories.ocean);
-            }
-        
-        }
-        
+       
     }
 
 }
