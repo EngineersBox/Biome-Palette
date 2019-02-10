8e5ac84dc4fa8bc22a1d515d0f2971818546bc23
Jack Kilrain <toastcraft.info@gmail.com>
2019-02-10 04:13:09 +0100

    Added global variables

--- src/me.engineersbox.menuinv/Main.java
+++ src/me.engineersbox.menuinv/Main.java
@@ -6,6 +6,9 @@ import java.util.List;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.block.Biome;
+import org.bukkit.block.Block;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -16,11 +19,19 @@ public class Main extends JavaPlugin implements Listener {
 	public static FileConfiguration config;
 	public static File cfile;
 	
-	//Global Strings
+	//Globals
 	public static String biome = "";
 	public static String prefix = ChatColor.DARK_RED + "[" + ChatColor.GOLD + "BlockPalette" + ChatColor.DARK_RED + "] ";
-	public static Integer getB = 0;
-	public static Integer chunkinfo = 0;
+	public static int getB = 0;
+	public static boolean arpl = false;
+	public static List<String> rplfromto;
+	public static List<Block> undolist;
+	public static Biome undobiome;
+	public static boolean undobool = false;
+	public static int poscount = 0;
+	public static Location loc1 = null;
+	public static Location loc2 = null;
+	public static int chunkinfo = 0;
 	public static String mater = "";
 	public static String inv = null;
 	public static int cinv = 0;
@@ -44,6 +55,9 @@ public class Main extends JavaPlugin implements Listener {
         getCommand("bp tool").setExecutor(new Commands());
         getCommand("bp setbiome").setExecutor(new Commands());
         getCommand("bp getbiome").setExecutor(new Commands());
+        getCommand("bp areareplace").setExecutor(new Commands());
+        getCommand("bp replaceto").setExecutor(new Commands());
+        getCommand("bp undo").setExecutor(new Commands());
         getCommand("bp biomelist").setExecutor(new Commands());
         getCommand("bp chunkinfo").setExecutor(new Commands());
         getCommand("bp version").setExecutor(new Commands());
